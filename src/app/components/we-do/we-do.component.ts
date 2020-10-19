4c46174f0c88c153c9d60cb952504e0dc4c9c8d2
xe-non <sithumsandeepap@gmail.com>
2020-10-20 00:32:38 +0530

    Feature/we do (#22)
    * add we-do component
    
    * add img-we-do image
    
    * mv we-do to component/
    
    * add bootstrap grid via cdn
    
    * reblance member n we-do has grid issue
    
    * Updated
    
    * Overlapping fixed
    
    Co-authored-by: Chamod Shehanka <hcsperera@outlook.com>

---
+++ src/app/components/we-do/we-do.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-we-do',
+  templateUrl: './we-do.component.html',
+  styleUrls: ['./we-do.component.css'],
+})
+export class WeDoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
