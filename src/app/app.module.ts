7739d74209b751a7ab6ca99ba777d8896125c7ad
xe-non <sithumsandeepap@gmail.com>
2020-10-20 00:45:25 +0530

    Feature/ceo (#24)
    * add: ceo comp
    
    * add: mr flutter img
    
    * set: mr flutter; grid has an issue
    
    * Overlapping fixed
    
    Co-authored-by: Chamod Shehanka <hcsperera@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { MemberComponent } from './components/member/member.component';
+import { CeoComponent } from './components/ceo/ceo.component';
 import { WeDoComponent } from './components/we-do/we-do.component';
 
 @NgModule({
@@ -20,6 +21,7 @@ import { WeDoComponent } from './components/we-do/we-do.component';
     ContactComponent,
     HomeComponent,
     MemberComponent,
+    CeoComponent,
     WeDoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
---
+++ src/app/components/ceo/ceo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ceo',
+  templateUrl: './ceo.component.html',
+  styleUrls: ['./ceo.component.css'],
+})
+export class CeoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
