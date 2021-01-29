c84e65ea3d2cbe0d4d93276c024c6256ff3a03e2
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-29 13:16:56 +0530

    podcast section move to the separate component

---
+++ src/app/components/home/podcastsection/podcastsection.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-podcastsection',
+  templateUrl: './podcastsection.component.html',
+  styleUrls: ['./podcastsection.component.css'],
+})
+export class PodcastsectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
