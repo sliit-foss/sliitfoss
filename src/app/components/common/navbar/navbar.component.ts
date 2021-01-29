411e1cf8a831837cb96ad486ddf8eee066dd76f2
Dureksha Wasala <37532612+dcjc29@users.noreply.github.com>
2021-01-29 14:24:48 +0530

    Fixed Navbar Issue (#148)

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +9,17 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  navbarVariable = false;
+  @HostListener('document:scroll')
+  scrollFunction() {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      this.navbarVariable = true;
+    } else {
+      this.navbarVariable = false;
+    }
+  }
 }
