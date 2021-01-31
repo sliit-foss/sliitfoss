4f6f7d6e5bc13e833fc8bb132c99275dea51d3f3
Dilshan97 <dilshanramesh81@gmail.com>
2021-02-01 00:46:51 +0530

    #164 fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,11 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'corrected' }),
+    RouterModule.forRoot(routes, {
+      relativeLinkResolution: 'corrected',
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }),
   ],
   exports: [RouterModule],
 })
