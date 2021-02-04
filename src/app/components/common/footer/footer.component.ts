1b0b60deb6908290a81467435f5ec29a849179dd
Ahamed Safnaj <safnaj99@gmail.com>
2021-02-04 19:14:58 +0530

    Route Issue Fixed & Navbar Improved (#194)
    * foss logo added
    
    * Alignment Issues Fixed
    
    * Bootstrap Added
    
    * Modified
    
    * Curved design size fixed and few changes
    
    * bootstrap js added
    
    * navbar toggle added
    
    * past events data
    
    * Past Events Carousel
    
    * Carousel & Home Page Improved
    
    * Events Component Done
    
    * https://github.com/Safnaj/sliitfoss.git
    
    * scripts modified
    
    * image resized
    
    * font awesome CDN added back
    
    * minor changes & fixes
    
    * Navbar Toggle Works and UI Fixes
    
    * Nav link active fixed
    
    * board details  json
    
    * Board Members Done (#54)
    
    * Board Component Improved
    
    * Asel's Profile
    
    * About Modofied
    
    * UI Changes
    
    * Webinar Data
    
    * Logo Size Cropped
    
    * Webinars Component Added
    
    * Conflicts Resolved
    
    * Route to Component
    
    * 404 Route Added Back
    
    * Changed to Reactive Form
    
    * Speaker Name Removed
    
    * Card height reduced
    
    * Fix #190
    
    Co-authored-by: Chamod Shehanka <hcsperera@outlook.com>

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  subscribeForm: FormGroup;
+
+  constructor(private form: FormBuilder) {
+    this.subscribeForm = this.form.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
   ngOnInit(): void {}
 
   getCurrentYear(): number {
     return new Date().getFullYear();
+  }
+
+  subscribe() {
+    console.log(this.subscribeForm.value);
   }
 }
