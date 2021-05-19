24f1ac33418b2a1623419fca0cfde9415decfb74
Shehanka <hcsperera@outlook.com>
2021-05-19 15:23:15 +0530

    Firebase Added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,14 @@ export const environment = {
   emailServiceId: 'service_664o6wc',
   emailTemplateId: 'template_1nhqybf',
   emailUserId: 'user_WyMiG19mwb6vTGJwqJq5C',
+  firebase: {
+    apiKey: 'AIzaSyAVm5DX0zZ_Tf6YJrgG8H4caFtEvXymk2M',
+    authDomain: 'sliitfoss-ac5d9.firebaseapp.com',
+    databaseURL: 'https://sliitfoss-ac5d9.firebaseio.com',
+    projectId: 'sliitfoss-ac5d9',
+    storageBucket: 'sliitfoss-ac5d9.appspot.com',
+    messagingSenderId: '318604026351',
+    appId: '1:318604026351:web:4b7544f0fef233bbb8561a',
+    measurementId: 'G-JFQE0NQ3FV',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAVm5DX0zZ_Tf6YJrgG8H4caFtEvXymk2M',
+    authDomain: 'sliitfoss-ac5d9.firebaseapp.com',
+    databaseURL: 'https://sliitfoss-ac5d9.firebaseio.com',
+    projectId: 'sliitfoss-ac5d9',
+    storageBucket: 'sliitfoss-ac5d9.appspot.com',
+    messagingSenderId: '318604026351',
+    appId: '1:318604026351:web:4b7544f0fef233bbb8561a',
+    measurementId: 'G-JFQE0NQ3FV',
+  },
 };
 
 /*
