a4845b4f68e2502a914036245bb0ba7b529a7b7d
Akalanka Perera <akalanka@parallax.lk>
2022-02-27 13:47:31 +0530

    Revert "Hotfix/build script (#329)"
    This reverts commit cc244f5ba0e5f352794d0280667b0fd5bb5ccb71.

---
+++ scripts/setEnv.ts
@@ -0,0 +1,18 @@
+import { writeFile } from 'fs';
+
+import 'dotenv/config';
+
+const targetPath = `./src/environments/environment.prod.ts`;
+const environmentFileContent = `
+export const environment = {
+    production: true,
+    firebaseConfig: "${process.env.FIREBASE_WEB_CONFIG}",
+    emailUserId: "${process.env.emailUserId}",
+    emailTemplateId: "${process.env.emailTemplateId}",
+    emailServiceId: "${process.env.emailServiceId}",
+}`;
+
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) console.log(err);
+  console.log(`Wrote variables to ${targetPath}`);
+});
