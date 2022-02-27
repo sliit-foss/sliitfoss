e7eed0a6f60c139e7bae89488ec3e8972bd57863
Akalanka Perera <akalanka@parallax.lk>
2022-02-28 00:26:52 +0530

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  emailServiceId: 'service_664o6wc',
-  emailTemplateId: 'template_1nhqybf',
-  emailUserId: 'user_WyMiG19mwb6vTGJwqJq5C',
-  firebaseConfig: '',
+  firebaseConfig:
+    'ew0KICAiYXBpS2V5IjogIkFJemFTeUFWbTVEWDB6Wl9UZjZZSnJnRzhINGNhRnRFdlh5bWsyTSIsDQogICJhdXRoRG9tYWluIjogInNsaWl0Zm9zcy1hYzVkOS5maXJlYmFzZWFwcC5jb20iLA0KICAiZGF0YWJhc2VVUkwiOiAiaHR0cHM6Ly9zbGlpdGZvc3MtYWM1ZDkuZmlyZWJhc2Vpby5jb20iLA0KICAicHJvamVjdElkIjogInNsaWl0Zm9zcy1hYzVkOSIsDQogICJzdG9yYWdlQnVja2V0IjogInNsaWl0Zm9zcy1hYzVkOS5hcHBzcG90LmNvbSIsDQogICJtZXNzYWdpbmdTZW5kZXJJZCI6ICIzMTg2MDQwMjYzNTEiLA0KICAiYXBwSWQiOiAiMTozMTg2MDQwMjYzNTE6d2ViOjRiNzU0NGYwZmVmMjMzYmJiODU2MWEiLA0KICAibWVhc3VyZW1lbnRJZCI6ICJHLUpGUUUwTlEzRlYiDQp9DQo=',
+  emailUserId: 'undefined',
+  emailTemplateId: 'undefined',
+  emailServiceId: 'undefined',
 };
