a426539075c3dc4149e1dbba7fb49e9c6c249b2f
Akalanka Perera <akalanka@parallax.lk>
2022-02-28 12:05:30 +0530

    Minor fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  firebaseConfig:
-    'ew0KICAiYXBpS2V5IjogIkFJemFTeUFWbTVEWDB6Wl9UZjZZSnJnRzhINGNhRnRFdlh5bWsyTSIsDQogICJhdXRoRG9tYWluIjogInNsaWl0Zm9zcy1hYzVkOS5maXJlYmFzZWFwcC5jb20iLA0KICAiZGF0YWJhc2VVUkwiOiAiaHR0cHM6Ly9zbGlpdGZvc3MtYWM1ZDkuZmlyZWJhc2Vpby5jb20iLA0KICAicHJvamVjdElkIjogInNsaWl0Zm9zcy1hYzVkOSIsDQogICJzdG9yYWdlQnVja2V0IjogInNsaWl0Zm9zcy1hYzVkOS5hcHBzcG90LmNvbSIsDQogICJtZXNzYWdpbmdTZW5kZXJJZCI6ICIzMTg2MDQwMjYzNTEiLA0KICAiYXBwSWQiOiAiMTozMTg2MDQwMjYzNTE6d2ViOjRiNzU0NGYwZmVmMjMzYmJiODU2MWEiLA0KICAibWVhc3VyZW1lbnRJZCI6ICJHLUpGUUUwTlEzRlYiDQp9DQo=',
+  firebaseConfig: '',
   emailUserId: 'undefined',
   emailTemplateId: 'undefined',
   emailServiceId: 'undefined',
