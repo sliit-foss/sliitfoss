0e4c9fad6459909c076201d8d9b93b2a21869de6
Akalanka Perera <akalanka@parallax.lk>
2022-04-26 20:28:35 +0530

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,11 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getDatabase, ref } from 'firebase/database';
 import { environment } from 'src/environments/environment.prod';
 
 const firebaseConfig = JSON.parse(atob(environment.firebaseConfig));
 
 initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+signInAnonymously(auth);
 
 export const db = getFirestore();
 export const realtimeDB = ref(getDatabase());
