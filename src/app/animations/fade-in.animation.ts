dbf9f8b975555323217ef8337e24efaa601c6252
Akalanka Perera <akalankaperera128@gmail.com>
2023-03-18 22:13:22 +0530

    Fixed white bg transition glitch (#398)

--- src/app/animations/fade-in.animation.ts
+++ src/app/animations/fade-in.animation.ts
@@ -9,28 +9,10 @@ import {
 export const FadeInAnimation = trigger('routeAnimations', [
   transition('* => *', [
     query(
-      ':enter',
-      [
-        style({
-          opacity: 0,
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          background: '#fff',
-          height: '100vh',
-          'z-index': 10,
-        }),
-      ],
-      { optional: true }
-    ),
-
-    query(
       ':leave',
       [style({ opacity: 1 }), animate('0.1s', style({ opacity: 0 }))],
       { optional: true }
     ),
-
     query(
       ':enter',
       [style({ opacity: 0 }), animate('0.1s', style({ opacity: 1 }))],
